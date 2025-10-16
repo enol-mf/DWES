54ff1adb2a5cbbaf08fc1e36202d5f5dffa0110f
enol-mf <enolmarques7@gmail.com>
2025-10-16 12:29:34 +0200

    ejs ut4

---
+++ UT4/pruebas/pruebaNums2.php
@@ -0,0 +1,51 @@
+<?php
+
+    if (isset($_POST['num1']) && isset($_POST['num2'])) {
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];  
+        $resultado = $num1 + $num2;
+
+        echo <<<_END
+        <html>
+        <head>
+            <title>Suma numeros</title>
+        </head>
+        <body>
+            
+            <form action="pruebaNums2.php" method="post">
+            <h2>Cliente</h2>
+            <label>num1</label> <input type="text" name="num1">
+            <br><br>
+            <label>num2</label> <input type="text" name="num2">
+            <br><br>
+            <input type="submit" value="Sumar">
+            </form>
+            <p> El resultado es $resultado </p>
+
+        </body>
+        </html>
+        _END;
+
+    } else
+    
+    echo <<<_END
+    <html>
+    <head>
+        <title>Suma numeros</title>
+    </head>
+    <body>
+        
+        <form action="pruebaNums2.php" method="post">
+        <h2>Cliente</h2>
+        <label>num1</label> <input type="text" name="num1">
+        <br><br>
+        <label>num2</label> <input type="text" name="num2">
+        <br><br>
+        <input type="submit" value="Sumar">
+        </form>
+
+    </body>
+    </html>
+    _END;
+
+?>
