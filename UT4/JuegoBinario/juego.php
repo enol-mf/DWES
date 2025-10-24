f0a11d2390100ddcda0e1d21eefda8112fb30245
enol-mf <enolmarques7@gmail.com>
2025-10-24 14:14:34 +0200

    ejs ut4

--- UT4/JuegoBinario/juego.php
+++ UT4/JuegoBinario/juego.php
@@ -6,6 +6,37 @@ for ($i = 0; $i < 4; $i++) {
     $numAleatorio[$i] = rand(0, 1);
 }
 
-print_r($numAleatorio);
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Adivina el número binario!</h1>
+
+    <label for="">Número en binario: </label> <p>
+
+    <?php
+
+    for ($i = 0; $i < 4; $i++) {
+        echo $numAleatorio[$i];
+    }
+?>
+    </p>
+    <br>
+    <div class="contenedor">
+        <img src="8.JPG" alt="" height="150px">
+        <img src="4.JPG" alt="" height="150px">
+        <img src="2.JPG" alt="" height="150px">
+        <img src="1.JPG" alt="" height="150px">
+    </div>
+    <br>
+    <label for="">Número decimal: </label> <input type="text" class="respuesta">
+</body>
+</html>
+
+
+
+
