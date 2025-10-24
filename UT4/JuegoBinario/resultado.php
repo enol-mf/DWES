a9a46faadc815a8510dcf1d6fc846a18c553a0f9
enol-mf <enolmarques7@gmail.com>
2025-10-24 23:57:36 +0200

    Ejercicio Juego Binario

--- UT4/JuegoBinario/resultado.php
+++ UT4/JuegoBinario/resultado.php
@@ -2,6 +2,13 @@
 
 session_start();
 
+$binario = $_SESSION['numAleatorio'];
+$respuesta = $_POST['respuesta'];
 
+if (bindec($binario) == $respuesta) {
+    echo "La respuesta es correcta";
+} else {
+    echo "La respueta no es correcta";
+}
 
 ?>
