99a4120d96aa50c57f21cb58710316b9ab4d0eeb
enol-mf <enolmarques7@gmail.com>
2025-11-10 10:44:15 +0100

    ej JuegoSimon con BDD

--- UT4/JuegoSimón/teoría.php
+++ UT4/JuegoSimón/teoría.php
@@ -18,31 +18,31 @@
 ?>
 
 <?php // query-mysqli.php
- require_once 'login.php';
- $connection = new mysqli($hn, $un, $pw, $db);
- if ($connection->connect_error) die("Fatal Error");
- $query = "SELECT * FROM classics";
- $result = $connection->query($query);
- if (!$result) die("Fatal Error");
- $rows = $result->num_rows;
- for ($j = 0 ; $j < $rows ; ++$j)
- {
- $result->data_seek($j);
- echo 'Author: ' .htmlspecialchars($result->fetch_assoc()['author']) .'<br>';
- $result->data_seek($j);
- echo 'Title: ' .htmlspecialchars($result->fetch_assoc()['title']) .'<br>';
- $result->data_seek($j);
- echo 'Category: ' .htmlspecialchars($result->fetch_assoc()['category']) .'<br>';
- $result->data_seek($j);
- echo 'Year: '. htmlspecialchars($result->fetch_assoc()['year'])
+require_once 'login.php';
+$connection = new mysqli($hn, $un, $pw, $db);
+if ($connection->connect_error) die("Fatal Error");
+$query = "SELECT * FROM classics";
+$result = $connection->query($query);
+if (!$result) die("Fatal Error");
+$rows = $result->num_rows;
+for ($j = 0 ; $j < $rows ; ++$j)
+{
+$result->data_seek($j);
+echo 'Author: ' .htmlspecialchars($result->fetch_assoc()['author']) .'<br>';
+$result->data_seek($j);
+echo 'Title: ' .htmlspecialchars($result->fetch_assoc()['title']) .'<br>';
+$result->data_seek($j);
+echo 'Category: ' .htmlspecialchars($result->fetch_assoc()['category']) .'<br>';
+$result->data_seek($j);
+echo 'Year: '. htmlspecialchars($result->fetch_assoc()['year'])
 .'<br>';
- $result->data_seek($j);
- echo 'ISBN: ' .htmlspecialchars($result->fetch_assoc()['isbn'])
+$result->data_seek($j);
+echo 'ISBN: ' .htmlspecialchars($result->fetch_assoc()['isbn'])
 .'<br><br>';
- } 
- ?>
+} 
+?>
 
- <?php
+<?php
 $hn = 'localhost:3307';
 $db = 'bdsimon';
 $un = 'root';
@@ -51,12 +51,12 @@ $connection = new mysqli($hn, $un, $pw, $db);
 
 $query = "SELECT Nombre FROM usuarios";
 
-     if ($connection->connect_error) die("Fatal Error");
-     $result = $connection->query($query);
-     if (!$result) die("Fatal Error");
-     $rows = $result->num_rows;
-     for ($j = 0 ; $j < $rows ; ++$j) {
+    if ($connection->connect_error) die("Fatal Error");
+    $result = $connection->query($query);
+    if (!$result) die("Fatal Error");
+    $rows = $result->num_rows;
+    for ($j = 0 ; $j < $rows ; ++$j) {
         $result->data_seek($j);
         echo 'usuarios: ' .htmlspecialchars($result->fetch_assoc()['Nombre']) .'<br>';
-     }
+    }
 ?>
